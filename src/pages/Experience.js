be37fc85c295e2a95c17cd82fc5df74fb56c5078
AdityaVishwakarma22 <22avkvishwakarma22@gmail.com>
2024-03-13 21:36:52 +0530

    experience points

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -9,11 +9,14 @@ function Experience() {
         <div className="experience-info">
           <h2 className="exp-heading">
             Freshdigital ( Intern ){" "}
-            <span className="button">Jul 2022 - Nov 2022</span>{" "}
+            <span className="button">Jan 2022 - Nov 2022</span>{" "}
           </h2>
           <ul className="responsibilities-list">
-            <li>Used agile software development frameworks, e.g., SCRUM.</li>
-            <li> Integrated React.js plugins, lifecycle methods and hooks</li>
+            <li>
+              {" "}
+              Implemented React.js plugins, lifecycle methods, and hooks to
+              enhance website functionality.
+            </li>
             <li>Built responsive and cross platform website.</li>
             <li>
               {" "}
@@ -21,8 +24,19 @@ function Experience() {
               sources.
             </li>
             <li>
-              Maintained and build features of website – design patterns,
-              graphs, git workflow.
+              Managed and enhanced website features using industry-standard
+              design patterns, graphs, and git workflow for efficient code
+              management.
+            </li>
+            <li>
+              {" "}
+              Engaged in effective collaboration within a team of 5 members to
+              accomplish project goals and deliver high-quality results.
+            </li>
+            <li>
+              Demonstrated proficiency in agile software development
+              methodologies, including SCRUM, to ensure efficient project
+              management and rapid iteration cycles.
             </li>
           </ul>
         </div>
